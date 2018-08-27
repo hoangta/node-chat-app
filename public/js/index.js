c6cfbe10c521507815b0b37fbbd7fe4d19b86821
Hoang Ta <vinhhoang.ta@2359media.com>
2018-08-27 14:06:19 +0700

    [add] Emit new message.

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io()
+
+socket.on('connect', function() {
+    console.log('Connected to server!')
+})
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server!')
+})
+
+socket.on('newMessage', function(message) {
+    console.log('new message')
+    var p = document.createElement('p')
+    var node = document.createTextNode(`${message.from}: ${message.text}`)
+    p.appendChild(node)
+    document.getElementById('content').appendChild(p)
+})
--- server/server.js
+++ server/server.js
@@ -15,6 +15,12 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected!')
 
+    socket.on('createMessage', (message) => {
+        console.log(`${message.from} said ${message.text}`)
+        message.createAt = new Date().getTime()
+        io.emit('newMessage', message)
+    })
+
     socket.on('disconnect', (socket) => {
         console.log('User was disconnected')
     })
