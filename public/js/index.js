4e88d6ea987d70444540f692994c39979d9976d5
Hoang Ta <vinhhoang.ta@2359media.com>
2018-08-28 00:22:29 +0700

    [add] scroll to bottom if neccessary.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io()
 
+function scrollToBottom() {
+    var messages = $('#messages')
+    var newMessage = messages.children('li:last-child')
+    // var newMessage = messages.last()
+    var clientHeight = messages.prop('clientHeight')
+    var scrollTop = messages.prop('scrollTop')
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight()
+    var lastMessageHeight = newMessage.prev().innerHeight()
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight)
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server!')
 })
@@ -16,7 +31,8 @@ socket.on('newMessage', function(message) {
         from: message.from,
         createAt: time
     })
-    $('#room').append(html)
+    $('#messages').append(html)
+    scrollToBottom()
 })
 
 socket.on('newLocationMessage', function(message) {
@@ -27,7 +43,8 @@ socket.on('newLocationMessage', function(message) {
         from: message.from,
         createAt: time
     })
-    $('#room').append(html)
+    $('#messages').append(html)
+    scrollToBottom()
 })
 
 $('#message-form').on('submit', function(e) {
